72b54f23a53125fe0c8eb8d87a4f3dd6a89da7b4
ChungWenTing <cwtk159x37@gmail.com>
2024-09-11 11:29:21 +0800

    update: 更新Header 點擊連結

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="relative py-6 px-8 w-full bg-white shadow-[0px_3px_6px_0_rgba(0,0,0,0.16)]">
     <div class="flex justify-between">
-      <p class="text-[#52528c] font-bold text-2xl">表單設計</p>
+      <router-link :to="{ name: 'GeneralList' }" class="text-[#52528c] font-bold text-2xl"> 表單設計 </router-link>
       <div class="relative">
         <button class="text-[#333] text-2xl" type="button" @click.prevent="is_bar = !is_bar">
           {{ 'name' }}
@@ -11,8 +11,11 @@
           v-if="is_bar"
           class="absolute top-[calc(100%_+_8px)] left-1/2 p-2 min-w-[calc(100%_+_20px)] rounded-[10px] bg-white shadow-[0px_3px_6px_0_rgba(0,0,0,0.16)] -translate-x-1/2 text-lg"
         >
-          <li>
-            <router-link to="" class="py-2 px-3.5 border-b border-solid border-[#eee] text-nowrap hover:text-[#52528C] hover:font-bold">
+          <li @click.prevent="is_bar = false">
+            <router-link
+              :to="{ name: 'AccountSetting' }"
+              class="py-2 px-3.5 border-b border-solid border-[#eee] text-nowrap hover:text-[#52528C] hover:font-bold"
+            >
               帳戶設定
             </router-link>
           </li>
